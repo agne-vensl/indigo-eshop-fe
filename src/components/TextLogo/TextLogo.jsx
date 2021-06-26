c63e65bcfed30657a9975d34635004649f5eabc8
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-06-26 18:44:35 +0300

    text logo component

---
+++ src/components/TextLogo/TextLogo.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./TextLogo.style";
+
+const TextLogo = ({ children }) => <S.TextLogo>{children}</S.TextLogo>;
+
+export default TextLogo;
---
+++ src/components/TextLogo/TextLogo.style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const TextLogo = styled.div`
+  color: blue;
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 2rem;
+`;
