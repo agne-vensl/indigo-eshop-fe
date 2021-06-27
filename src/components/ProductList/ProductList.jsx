dd3b9cb74e13c60a834c70e24412b9b343044da7
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-06-27 17:29:45 +0300

    product list component

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./ProductList.style";
+
+const ProductList = ({ children }) => <S.ProductList>{children}</S.ProductList>;
+
+export default ProductList;
---
+++ src/components/ProductList/ProductList.style.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ProductList = styled.div`
+  width: 100%;
+
+  @media screen and (min-width: 660px) {
+    display: flex;
+    flex-wrap: wrap;
+
+    & > *:not(:nth-child(2n)) {
+      margin-right: 1rem;
+    }
+
+    & > * {
+      width: calc(50% - 1rem);
+    }
+  }
+
+  @media screen and (min-width: 830px) {
+    display: flex;
+    flex-wrap: wrap;
+
+    & > *:nth-child(3n) {
+      margin-right: 0;
+    }
+
+    & > *:not(:nth-child(3n)) {
+      margin-right: 1rem;
+    }
+
+    & > * {
+      width: calc(100% / 3 - 2rem / 3);
+    }
+  }
+`;
