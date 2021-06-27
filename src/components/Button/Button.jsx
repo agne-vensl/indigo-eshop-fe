111c7e55685e2c7a935e0225c1777a6096948966
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-06-28 00:19:13 +0300

    added button type

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import * as S from "./Button.style";
 
-const Button = ({ children, onClick }) => (
-  <S.Button onClick={onClick}>{children}</S.Button>
+const Button = ({ children, onClick, type }) => (
+  <S.Button type={type || "button"} onClick={onClick}>
+    {children}
+  </S.Button>
 );
 
 export default Button;
